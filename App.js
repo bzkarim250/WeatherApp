30e60e9f171dea96227c7a7fec2ad313cf52e3ef
Abdoulkalim Bazambanza <bzkarim250@gmail.com>
2022-03-31 18:04:31 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+function App() {
+  //function to generate current date
+  const dateBuilder=(d)=>
+  {
+    let months= ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+     "Saturday"];
+    let day=days[d.getDay()];
+    let date=d.getDate();
+    let month=months[d.getMonth()];
+    let year=d.getFullYear();
+    //const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+    return `${day} ${date} ${month} ${year}` 
+  }
+  return (
+    <div className="app">
+       <main className='main'>
+         <div className="search-box">
+           <input
+            type="text"
+            className='search-bar'
+            placeholder='Seaching...' 
+            />
+         </div>
+         <div className='location-box'>
+            <div className='location'>Rwamagana, Rwanda </div>
+            <div className='date'>{dateBuilder(new Date())}</div>
+         </div>
+         <div className="weather-box">
+           <div className="temp">
+             15°C
+           </div>
+           <div className="weather">Sunny</div>
+         </div>
+       </main>
+    </div>
+  );
+}
+
+export default App;
